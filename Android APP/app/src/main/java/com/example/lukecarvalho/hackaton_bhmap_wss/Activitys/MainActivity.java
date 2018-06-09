53b2bfe104cf7354f4b68a01f0fd22c744c59281
LukePgr <lucas.docs.mail@gmail.com>
2018-06-09 14:27:04 -0300

    Start Android Project

---
+++ Android APP/app/src/main/java/com/example/lukecarvalho/hackaton_bhmap_wss/Activitys/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lukecarvalho.hackaton_bhmap_wss.Activitys;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lukecarvalho.hackaton_bhmap_wss.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        startActivity(new Intent(this, LoginActivity.class));
+        
+    }
+}
