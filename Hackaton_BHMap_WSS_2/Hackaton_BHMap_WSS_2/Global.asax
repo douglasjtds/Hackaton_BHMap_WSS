c3e987bd419844dd850b0598a96ea5518dc923cb
Matheus Pires <matheuswith51@hotmail.com>
2018-06-09 12:17:07 -0300

    trying create project

---
+++ Hackaton_BHMap_WSS_2/Hackaton_BHMap_WSS_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hackaton_BHMap_WSS_2.Global" Language="C#" %>
---
+++ Hackaton_BHMap_WSS_2/Hackaton_BHMap_WSS_2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackaton_BHMap_WSS_2.Startup))]
+namespace Hackaton_BHMap_WSS_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
