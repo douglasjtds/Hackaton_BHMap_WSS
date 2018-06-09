f02f29051ca7c6c718338b044df3f2f055a01a7e
Douglas Tertuliano <douglasjtds@gmail.com>
2018-06-09 18:50:35 -0300

    deleting project and creating another - now it's working

---
+++ Hackaton_WSS/Hackaton_WSS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackaton_WSS.Startup))]
+namespace Hackaton_WSS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
