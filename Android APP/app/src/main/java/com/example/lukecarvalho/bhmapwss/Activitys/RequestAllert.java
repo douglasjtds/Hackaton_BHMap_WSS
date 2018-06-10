8ad854dd120c96879d7ac487415f0cfe76e04376
LukePgr <lucas.docs.mail@gmail.com>
2018-06-10 09:49:14 -0300

    Integraçcao com o Firebase e mais um monte de coisa

---
+++ Android APP/app/src/main/java/com/example/lukecarvalho/bhmapwss/Activitys/RequestAllert.java
@@ -0,0 +1,60 @@
+package com.example.lukecarvalho.bhmapwss.Activitys;
+
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.lukecarvalho.bhmapwss.Classes.FireDB;
+import com.example.lukecarvalho.bhmapwss.R;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RequestAllert extends AppCompatActivity {
+
+    private Button btn;
+    private String agentID;
+    private FireDB.Chamado call;
+    private FireDB fdb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_allert);
+
+        fdb = new FireDB();
+
+        Intent i = getIntent();
+        call = (FireDB.Chamado) i.getExtras().getSerializable("CHAMADO");
+        agentID = call.getId();
+        setTitle(call.getTitle());
+
+
+        TextView desk = findViewById(R.id.TVAlertDesc);
+        desk.setText(call.getDescription());
+
+        TextView local = findViewById(R.id.TVAlertLocal);
+        local.setText(call.getEndereco());
+
+        btn = findViewById(R.id.Close);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newAgente_Chamado();
+                finish();
+            }
+        });
+
+        FireDB fdb = new FireDB();
+        fdb.updateAgentLocation(agentID, (Double) i.getExtras().getSerializable("LAT"), (Double) i.getExtras().getSerializable("LON"));
+
+    }
+
+    public void newAgente_Chamado () {
+        fdb.writeNew_agentId_chamado_Id(agentID,call.id);
+    }
+
+
+}
---
+++ Android APP/app/src/main/java/com/example/lukecarvalho/bhmapwss/Classes/Notification.java
@@ -0,0 +1,19 @@
+package com.example.lukecarvalho.bhmapwss.Classes;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class Notification extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+
+        return null;
+    }
+}
